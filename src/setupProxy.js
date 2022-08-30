394b1c71c9fb7f1295264286b61b88262e1bef69
dipendrashrestha1281 <96989972+dipendrashrestha1281@users.noreply.github.com>
2022-08-30 16:25:45 -0500

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,8 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
 
-    app.use(proxy('/api', { target: 'http://localhost:8080/' }));
-    app.use(proxy('/account', { target: 'http://localhost:8081/' })); 
+    app.use(proxy('/api', { target: 'http://10.96.221.247:8080/' }));
+    app.use(proxy('/account', { target: 'http://10.103.3.186:8081/' })); 
 	
 	/*app.use(proxy('/api', { target: 'http://172.18.0.2:8080/' }));
 	app.use(proxy('/account', { target: 'http://172.18.0.3:8081/' }));
